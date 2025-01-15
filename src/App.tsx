25f65b22576d0fc3453ab69cccb2339b6bb3348b
LuizinhoGustavo <lggustavo329@gmail.com>
2025-01-15 18:33:14 -0300

    feat: Home desenvolvida

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
+import { FileUser, Github, Linkedin } from "lucide-react";
 import { SideBar } from "./Components/SideBar/SideBar";
 import "./index.css"
+import { ReactTyped } from "react-typed";
 
 export function App() {
   return (
-    <div>
-      <SideBar />
+    <div className="flex flex-row bg-dotted-pattern">
 
-      <div className="flex flex-col mx-auto justify-center text-center max-w-7xl">
-        <h1>Luiz Gustavo</h1>
+      {/* Conteúdos em  Div's para corrigir a sobreposição do conteúdo na Side Bar*/}
+      <div className="w-42">
+        <SideBar />
       </div>
-    </div>
+
+      <div className="flex flex-col mx-auto justify-center max-w-5xl">
+        <div className="h-screen flex flex-col gap-2 items-center justify-center">
+          <h1 className="font-bold text-5xl"><ReactTyped strings={['Luiz Gustavo', 'Desenvolvedor FrontEnd']} typeSpeed={200} backSpeed={30} loop />
+          </h1>
+          <p className="font-semibold text-xl">Console.log(“Desenvolvo aplicações Web!”) </p>
+
+          <div className="flex w-full flex-row justify-center gap-14">
+            <Linkedin size={40} strokeWidth={1.7} className="hover:text-orange-600 duration-500" />
+            <Github size={40} strokeWidth={1.7} className="hover:text-orange-600 duration-500" />
+            <FileUser size={40} strokeWidth={1.7} className="hover:text-orange-600 duration-500" />
+          </div>
+        </div>
+
+
+      </div>
+
+
+
+    </div >
   )
 }
--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -3,29 +3,27 @@ import { useState } from 'react';
 import { House, SquareUser, FolderKanban, FileJson2, BriefcaseBusiness, PanelRightOpen, PanelRightClose } from 'lucide-react'
 
 export function SideBar() {
-    const [isOpen, setIsOpen] = useState(false); // State to manage sidebar visibility
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggleSidebar = () => {
-        setIsOpen(prevState => !prevState); // Toggle the sidebar state
+        setIsOpen(prevState => !prevState);
     };
 
     return (
         <div className="flex">
+
             <aside
-                className={`h-full w-16 md:w-32 flex flex-col gap-10 justify-center items-center left-0 absolute border-2 border-gray-300 shadow-lg shadow-indigo-500/50 transition-transform duration-500 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+                className={`h-full w-16 fixed md:w-32 flex flex-col gap-10 justify-center items-center bg-white left-0 border-2 border-gray-300 shadow-lg shadow-indigo-500/50 transition-transform duration-500 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
             >
-                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><House size={40} /></a></div>
-                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><SquareUser size={40} /></a></div>
-                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><FolderKanban size={40} /></a></div>
-                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><FileJson2 size={40} /></a></div>
-                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><BriefcaseBusiness size={40} /></a></div>
+                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><House size={37} /></a></div>
+                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><SquareUser size={37} /></a></div>
+                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><FolderKanban size={37} /></a></div>
+                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><FileJson2 size={37} /></a></div>
+                <div className="text-center hover:text-orange-600 duration-500"><a href="#"><BriefcaseBusiness size={37} /></a></div>
             </aside>
 
             {/* Button positioned next to the sidebar */}
-            <button
-                className={`ml-4 p-2 z-10 ${isOpen ? 'translate-x-12 md:translate-x-28' : '-translate-x-3'} duration-700`}
-                onClick={toggleSidebar}
-            >
+            <button className={`ml-4 p-2 z-10 md:hidden ${isOpen ? 'translate-x-12 md:translate-x-28' : '-translate-x-3'} duration-700`} onClick={toggleSidebar} >
                 {isOpen ? <PanelRightClose /> : <PanelRightOpen />}
             </button>
         </div>
